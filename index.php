216edadda7bd61b64173d9af81bf04f0f098c197
Callum <220115122@aston.ac.uk>
2023-11-15 12:28:33 +0000

    commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="keywords"
+    content="HTML, CSS, JavaScript, PHP, Coding, Code, Web Development, Junior Web Development, Computer Science, Jobs, Hot Beans">
+  <title>The Thought Cabinet</title>
+  <link href="C:\Users\chalm\Documents\vsCode - portfolio 1\css\style.css" rel="stylesheet" type="text/css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
+  <link rel="apple-touch-icon" sizes="180x180"
+    href="C:\Users\chalm\Documents\vsCode - portfolio 1\images\logo\apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32"
+    href="/C:\Users\chalm\Documents\vsCode - portfolio 1\images\logo\favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16"
+    href="C:\Users\chalm\Documents\vsCode - portfolio 1\images\logo\favicon-16x16.png">
+  <link rel="manifest" href="C:\Users\chalm\Documents\vsCode - portfolio 1\images\logo\site.webmanifest">
+</head>
+
+<body>
+  <main id="contentholder">
+
+    <div id="main-header">
+      <h1>The Thought Cabinet</h1>
+
+      <div id="interactables">
+        <ul>
+          <li><a href="index.html" onclick="open">Home</a></li>
+          <li><a href="projects.html" onclick="open">Projects</a></li>
+          <li><a href="contact.html" onclick="open">Contact Us</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div id="spacing-x">
+      <h2><em><u>Choose your future. Choose us.</u></em></h2>
+    </div>
+
+    <div class="maincontent-x">
+
+      <div class="images" id="image1">
+        <img src="C:\Users\chalm\Documents\vsCode - portfolio 1\images\randomjava.jpg" alt="" width="85%"
+          height="100%"></img>
+      </div>
+
+      <div class="textstuff" id="textstuff1">
+        <u>
+          <h3>What we do:</h3>
+        </u>
+        <p>Previously we have had clients such as Aston University and Hot Beans to aid in the creation of their own and
+          their clients' websites, as well as designing portfolio tasks for first year CS students.</p>
+      </div>
+
+    </div>
+
+    <div id="spacing-x"></div>
+
+    <div class="maincontent-x">
+      <div id="heading">
+        <h2><u>Client Testimonials</u></h2>
+      </div>
+
+      <div class="textstuff" id="textstuff3">
+        <blockquote>"It was a pleasure working with other passionate web developers with The Thought Cabinet,
+          due to their aid on projects, both of our companies have been able to gain" - Steve, Hotbeans
+        </blockquote>
+      </div>
+
+      <div class="textstuff" id="textstuff4">
+        <blockquote>"The Thought Cabinet have helped our students to learn Java to a greater extent
+          with their intermidiate challenges that push the students to achieve more" - Aston University
+        </blockquote>
+      </div>
+
+    </div>
+
+    <div id="spacing-x"></div>
+
+    <div id="footer">
+      <p>Our Professional Profiles:</p>
+
+      <div id="footimages">
+
+        <a rel="business social" href="https://github.com" onclick="open"><img
+            src="C:\Users\chalm\Documents\vsCode - portfolio 1\images\github.png" alt="Github" width="5%"
+            height="5%"></img></a>
+
+        <a rel="business social" href="https://www.linkedin.com/?trk=people-guest_nav-header-logo" onclick="open"><img
+            src="C:\Users\chalm\Documents\vsCode - portfolio 1\images\linkedin.png" alt="LinkedIn" width="5%"
+            height="5%"></img></a>
+
+        <a rel="business social"
+          href="https://twitter.com/Twitter?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" onclick="open"><img
+            src="C:\Users\chalm\Documents\vsCode - portfolio 1\images\twitter.png" alt="Twitter" width="5%"
+            height="5%"></img></a>
+
+      </div>
+
+    </div>
+
+    <footer id="actualFooter">
+      <p>Callum Chalmers©</p>
+    </footer>
+
+  </main>
+
+
+  <script src="script.js"></script>
+</body>
+
+</html>
